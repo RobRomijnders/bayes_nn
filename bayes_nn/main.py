1a8c69b3eeb885f75fa749ec9084b5a60f6e69a3
rob <romijndersrob@gmail.com>
2017-12-30 12:17:41 +0100

    initial upload

---
+++ bayes_nn/main.py
@@ -0,0 +1,28 @@
+
+from bayes_nn.data_loader import Dataloader
+from bayes_nn.mc_methods import MC_sampling
+from bayes_nn.training import training_multi
+from bayes_nn.training_lang import training_lang
+
+
+def main():
+    save_path = 'saved_models_lang'
+    training_lang(save_path)
+
+    save_path = 'saved_models'
+    training_multi(save_path)
+
+    dataloader = Dataloader('data/raw')
+    test_batch = dataloader.sample_NCHW(dataset='test')
+
+    print('\nDo MC dropout\n')
+    MC_sampling('saved_models', test_batch, 'dropout')
+
+    print('\nDo MC multi\n')
+    MC_sampling('saved_models', test_batch, 'multi')
+
+    print('\nDo MC lang\n')
+    MC_sampling('saved_models_lang', test_batch, 'lang')
+
+if __name__ == "__main__":
+    main()
